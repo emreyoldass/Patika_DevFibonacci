57e66c7e55f6cc590cbe6d3c9eae7183be72c3a2
Emre Yoldaş <53980583+emreyoldass@users.noreply.github.com>
2022-07-03 13:58:13 +0300

    Proje dosyası ekle.

---
+++ FibonacciDizisiOrtalamaHesaplama/FibonacciDizisiOrtalamaHesaplama/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FibonacciDizisiOrtalamaHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> dizi = new List<float>();
+
+            fibonacci _fibonacci = new fibonacci();
+            float ort = 0;
+            float sonuc = 0;
+
+            Console.WriteLine("Derinlik giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sayi; i++)
+            {
+                dizi.Add(_fibonacci.createarray());
+            }
+            Console.Write("1,");
+            Console.WriteLine(string.Join(',', dizi.ToArray()));
+
+            for (int i = 0; i < sayi; i++)
+            {
+                ort += dizi[i];
+                sonuc = Queryable.Average(dizi.AsQueryable());
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Ortalama: " + sonuc);
+        }
+    }
+}
---
+++ FibonacciDizisiOrtalamaHesaplama/FibonacciDizisiOrtalamaHesaplama/fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciDizisiOrtalamaHesaplama
+{
+    internal class fibonacci
+    {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        public float createarray()
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            return b;
+        }
+    }
+}
+
